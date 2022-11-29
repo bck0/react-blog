690fe96d34bc95cbdef3b422429714faa947888a
Erik Faltynek <e_faltynek@utb.cz>
2022-11-29 15:05:14 +0100

    Completed

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,16 +1,35 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
+  const {
+    data: blog,
+    isLoading,
+    error,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
 
-  useEffect(() => {
-    console.log("test");
-  }, []);
-
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
-      <h2>Blog details - {id}</h2>
+      {isLoading && <p>Loading...</p>}
+      {error && <div> {error} </div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>by: {blog.author}</p>
+          <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
+        </article>
+      )}
     </div>
   );
 };
--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Erik");
+  const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    setIsLoading(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setIsLoading(false);
+      // history.go(-1);
+      history.push("/");
+    });
+  };
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Erik">Erik</option>
+          <option value="Jirka">Jirka</option>
+        </select>
+        {!isLoading && <button>Add blog</button>}
+        {isLoading && <button disabled>Adding blog...</button>}
+      </form>
     </div>
   );
 };
